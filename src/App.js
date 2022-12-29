e7b0f4d9a6d59e162facad368afb2b0ada3c4a38
unknown <anamulislamshamim117@gmail.com>
2022-12-29 18:43:52 +0600

    successfully completed constants, action and reducer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React, {  } from "react";
 import './App.css';
 
 function App() {
-  return (
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{  } </h1>
+      <button >Increment ++</button>
     </div>
   );
 }
 
 export default App;
+// 1. state - count: 0;
+// 2. actions - increment, decrement, reset
+// 3. reducer
+// 4. store
